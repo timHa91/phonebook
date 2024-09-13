392d73ea37b7eabc4826473699ed202d113ca416
timHa91 <tim.hartmann.91@web.de>
2024-09-13 14:03:56 +0200

    integrated front end

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
 
-
 app.use(cors());
 app.use(express.json());
 morgan.token('body',(req) => {
     return JSON.stringify(req.body);
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(express.static('dist'));
 
 let persons = [
     {
